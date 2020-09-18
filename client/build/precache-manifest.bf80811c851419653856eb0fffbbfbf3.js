42fd3b3c18c8b97eb684cc194a5bdea86f48e859
Joseph Kim <jk851227@gmail.com>
2020-09-18 16:22:39 -0700

    build

---
+++ client/build/precache-manifest.bf80811c851419653856eb0fffbbfbf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ed4092fd7659840ce81864665fd6ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98d6fc83819111a90dff",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c664accf0ed8b9c40dd1",
+    "url": "/static/js/2.6a3437b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a3437b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98d6fc83819111a90dff",
+    "url": "/static/js/main.598234f1.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
